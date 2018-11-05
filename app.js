05f98d1c426cf384df6dbcd1c68e64ead0cb9750
antenorr <roudyantenor@gmail.com>
2018-11-05 15:53:27 -0500

    made connection betwen react and express - express and mongo already set up

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fetch = require("node-fetch");
 
 const productRoutes = require('./api/routes/products');//  here we bring in the product file
 
@@ -16,6 +18,9 @@ mongoose.connect(`mongodb://rantenor:${process.env.MONGO_ATLAS_PW}@cluster0-shar
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));// true for embeds
 app.use(bodyParser.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //  ***CORS handling business - adding errors to the response */
 app.use((req, res, next) => {
@@ -32,6 +37,16 @@ app.use((req, res, next) => {
 //  **************************************************** */
 //  Routes that will handle the requests
 app.use('/products', productRoutes);
+
+app.use('/api/spacex', (req, res) => {
+  fetch(`https://api.spacexdata.com/v3/launches/`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(data => {
+      res.json(data)
+    })
+});
 
 
 //  Below we have error handling from #4 19-30
@@ -51,3 +66,12 @@ app.use((error, req, res, next) => {
 
 module.exports = app;
 //  this express app will function as a request handler!
+/*
+"devDependencies": {
+    "eslint": "^5.8.0",
+        "eslint-config-airbnb": "^17.1.0",
+            "eslint-plugin-import": "^2.14.0",
+                "eslint-plugin-jsx-a11y": "^6.1.2",
+                    "eslint-plugin-react": "^7.11.1"
+},
+*/
---
+++ client/src/App.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  // Initialize state
+  state = { 
+    products: [],
+    nasaData: null
+   }
+
+  // Fetch passwords after first mount
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = () => {
+    // Get the passwords and store them in state
+    fetch('/products')
+      .then(res => res.json())
+      .then(products => { 
+        this.setState({ products });
+        console.log(products)
+    });
+  }
+
+  nasaData = () => {
+
+    fetch(`/api/spacex`, { method: 'GET' })
+      .then((response) => {
+        return response.json();
+      })
+      .then((nasaData) => {
+        this.setState({ nasaData })
+        console.log(nasaData)
+        // console.log(JSON.stringify(nasaData));
+      });
+
+  }
+
+  render() {
+    const { products, nasaData } = this.state;
+    let shipNames = null;
+    if (nasaData) {
+      shipNames = nasaData.map((el, idx) => {
+       return  <p key={idx}>{el.mission_name}</p>
+      })
+    }
+
+    return (
+      <div>
+
+        <button
+          className="more button"
+          onClick={this.nasaData}>
+          Get More
+        </button> 
+        <div>
+          {products.message}
+        </div>
+        <div>
+          {shipNames}
+        </div>
+
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
